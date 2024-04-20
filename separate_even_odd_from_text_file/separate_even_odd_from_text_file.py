8d0c0d5a7df50607be4f56c0deebb1f08a796067
glnnluciano <glnnluciano@gmail.com>
2024-04-20 20:43:02 +0800

    END OF PROGRAM

--- separate_even_odd_from_text_file/separate_even_odd_from_text_file.py
+++ separate_even_odd_from_text_file/separate_even_odd_from_text_file.py
@@ -7,5 +7,12 @@ with open("numbers.txt", "r") as numbers_file:
         if int(line) % 2 == 0:
 
 # write even numbers to even.txt
+            with open("even.txt", "a") as even_file:
+                even_file.write(line + "\n")
+
 # write odd numbers to odd.txt
+        else:
+            with open("odd.txt", "a") as odd_file:
+                odd_file.write(line + "\n")
+                
 # END OF PROGRAM
