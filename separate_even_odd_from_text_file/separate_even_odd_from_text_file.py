58f3c4e16e4a2eee3cc48eaeca080ea4268e5ee6
glnnluciano <glnnluciano@gmail.com>
2024-04-20 20:28:19 +0800

    add if statement

--- separate_even_odd_from_text_file/separate_even_odd_from_text_file.py
+++ separate_even_odd_from_text_file/separate_even_odd_from_text_file.py
@@ -2,8 +2,10 @@
 with open("numbers.txt", "r") as numbers_file:
     for line in numbers_file:
         line = line.strip()
-        print(line)
+
 # separate odd and even numbers
+        if int(line) % 2 == 0:
+
 # write even numbers to even.txt
 # write odd numbers to odd.txt
 # END OF PROGRAM
