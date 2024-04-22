843c543659b0d74da473e7fc5e566ad092d8bd87
glnnluciano <glnnluciano@gmail.com>
2024-04-22 21:47:40 +0800

    add if statement and initialization

--- get_student_with_highest_gwa_from_text_file/find_highest_gwa.py
+++ get_student_with_highest_gwa_from_text_file/find_highest_gwa.py
@@ -1,9 +1,18 @@
 # open the text file and read per line
 with open("class_record.txt", "r") as class_record:
+
+# initialize the variable 
+    highest_gwa = 0
+
     for line in class_record:
-        
+
 # separate the name and gwa
         student_name, gwa = line.strip().split(",")
         gwa = float(gwa)
+
 # find the highest gwa
+    if gwa > highest_gwa:
+        highest_gwa = gwa
+        highest_gwa_student = student_name
+        
 # print the name and gwa of the student with the highest gwa
