d35dd449e9eade4c837bcc8b20454e9b1552d682
glnnluciano <glnnluciano@gmail.com>
2024-04-22 21:24:27 +0800

    open class record file and read it per line

---
+++ get_student_with_highest_gwa_from_text_file/find_highest_gwa.py
@@ -0,0 +1,7 @@
+# open the text file and read per line
+with open("class_record.txt", "r") as class_record:
+    for line in class_record:
+        print(line)
+# separate the name and gwa
+# find the highest gwa
+# print the name and gwa of the student with the highest gwa
