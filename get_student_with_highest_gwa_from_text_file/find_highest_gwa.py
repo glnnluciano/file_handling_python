3c90604d3a2dd40aafe40d1e38114215ae13c2be
glnnluciano <glnnluciano@gmail.com>
2024-04-22 21:36:57 +0800

    separate name and gwa, and convert gwa to float

--- get_student_with_highest_gwa_from_text_file/find_highest_gwa.py
+++ get_student_with_highest_gwa_from_text_file/find_highest_gwa.py
@@ -1,7 +1,9 @@
 # open the text file and read per line
 with open("class_record.txt", "r") as class_record:
     for line in class_record:
-        print(line)
+        
 # separate the name and gwa
+        student_name, gwa = line.strip().split(",")
+        gwa = float(gwa)
 # find the highest gwa
 # print the name and gwa of the student with the highest gwa
