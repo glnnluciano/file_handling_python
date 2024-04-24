25859934d08b429060724f16e982770d742c8f96
glnnluciano <glnnluciano@gmail.com>
2024-04-24 18:42:24 +0800

    add loop; elif c, break; else, print invalid

--- text_compiler/text_compiler.py
+++ text_compiler/text_compiler.py
@@ -1,26 +1,34 @@
+# add loop
+while True:    
 # if want to enter data in mylife.txt file(y = yes, n = no), 
-user_input = input("Do you want to enter data in mylife.txt file? (y/n): ")
+    user_input = input("Do you want to enter data in mylife.txt file? (y/n): ")
 
-# if yes, open/create mylife.txt file, use append mode
-if user_input.lower() == "y":
-    with open("mylife.txt", "a") as my_life:
-        my_life.write(input("Enter line: ") + "\n")
-
-# if no,
-elif user_input.lower() == "n":
-
-    # input if want to read, overwrite, close mylife.txt file (r = read, s = startover, c = close)
-    read_start_over_close = input("Do you want to read (r), startover (s), or close (c) mylife.txt file?: ")
-
-    # if r, open mylife.txt file, read
-    if read_start_over_close.lower() == "r":
-        with open("mylife.txt", "r") as my_life:
-            for line in my_life:
-                print(line.strip())
-        
-        # if s, open mylife.txt file, overwrite
-    elif read_start_over_close.lower() == "s":
-        with open("mylife.txt", "w") as my_life:
+    # if yes, open/create mylife.txt file, use append mode
+    if user_input.lower() == "y":
+        with open("mylife.txt", "a") as my_life:
             my_life.write(input("Enter line: ") + "\n")
+
+    # if no,
+    elif user_input.lower() == "n":
+
+        # input if want to read, overwrite, close mylife.txt file (r = read, s = startover, c = close)
+        read_start_over_close = input("Do you want to read (r), startover (s), or close (c) mylife.txt file?: ")
+
+        # if r, open mylife.txt file, read
+        if read_start_over_close.lower() == "r":
+            with open("mylife.txt", "r") as my_life:
+                for line in my_life:
+                    print(line.strip())
             
+        # if s, open mylife.txt file, overwrite
+        elif read_start_over_close.lower() == "s":
+            with open("mylife.txt", "w") as my_life:
+                my_life.write(input("Enter line: ") + "\n")
+
         # if c, break
+        elif read_start_over_close.lower() == "c":
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+    
