671ef0990848e416b88ad29a1a23ea1d355febb1
glnnluciano <glnnluciano@gmail.com>
2024-04-24 18:04:05 +0800

    if statement, append

--- text_compiler/text_compiler.py
+++ text_compiler/text_compiler.py
@@ -1,8 +1,12 @@
-# open/create mylife.txt file
-with open("mylife.txt", "w") as my_life:
-        print(my_life.write("Hello World!"))
 # if want to enter data in mylife.txt file(y = yes, n = no), 
-# if yes, use append mode
+user_input = input("Do you want to enter data in mylife.txt file? (y/n): ")
+
+# if yes, open/create mylife.txt file, use append mode
+if user_input.lower() == "y":
+        with open("mylife.txt", "a") as my_life:
+            my_life.write(input("Enter line: ") + "\n")
 # if no,
-# if want to read data from mylife.txt file (r = read), use read mode, 
-# if want to overwrite data in mylife.txt file, use write mode (s = startover)
+    # input if want to read, overwrite, close mylife.txt file (r = read, s = startover, c = close)
+        # if r, open mylife.txt file, read
+        # if s, open mylife.txt file, overwrite
+        # if c, break
