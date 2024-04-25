72bf3749ab5613bf0b1ec60c18c8dccb6c92e882
glnnluciano <glnnluciano@gmail.com>
2024-04-25 13:37:46 +0800

    convert string in line to integers

--- squared_even_and_cubed_odd/squared_even_and_cubed_odd.py
+++ squared_even_and_cubed_odd/squared_even_and_cubed_odd.py
@@ -1,9 +1,10 @@
 # open and read integer.txt
 with open("integers.txt", "r") as integer_file:
-    for line in integer_file:
-        print(line)
+    for line in integer_file: 
 
 # convert line into integer
+        line = int(line.strip())
+        
 # if number is even, square it and append in double.txt
     # open and append line to double.txt
 # if number is odd, cube it and append in triple.txt
