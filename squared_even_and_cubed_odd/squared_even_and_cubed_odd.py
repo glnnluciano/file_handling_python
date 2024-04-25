db9623051b58ff371d8b482bf48c8e963fd66850
glnnluciano <glnnluciano@gmail.com>
2024-04-25 13:35:03 +0800

    open and read text file

---
+++ squared_even_and_cubed_odd/squared_even_and_cubed_odd.py
@@ -0,0 +1,11 @@
+# open and read integer.txt
+with open("integers.txt", "r") as integer_file:
+    for line in integer_file:
+        print(line)
+
+# convert line into integer
+# if number is even, square it and append in double.txt
+    # open and append line to double.txt
+# if number is odd, cube it and append in triple.txt
+    # open and append line to triple.txt 
+# END OF PROGRAM
