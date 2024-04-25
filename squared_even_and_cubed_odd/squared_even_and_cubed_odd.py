65bf2f2c418d677e79e1d0ca3285390e43966ada
glnnluciano <glnnluciano@gmail.com>
2024-04-25 15:18:55 +0800

    if e, exit; else, invalid input; and add  while loop

--- squared_even_and_cubed_odd/squared_even_and_cubed_odd.py
+++ squared_even_and_cubed_odd/squared_even_and_cubed_odd.py
@@ -25,20 +25,31 @@ with open("integers.txt", "r") as integer_file:
             with open("triple.txt", "a") as odd_file:
                 odd_file.write(str(line) + "\n")
 
-# if want to print text files (s = squared even, c = cubed odd, e = exit)
-user_input = input("Do you want to read the text files? (s = squared even, c = cubed odd, e = exit): ")
+# loop
+while True:
 
-# if s, read double.txt and print
-if user_input.lower() == "s":
-    with open("double.txt", "r") as double_text:
-        for line in double_text:
-            print(line.strip())
+    # if want to print text files (s = squared even, c = cubed odd, e = exit)
+    user_input = input("Do you want to read the text files? (s = squared even, c = cubed odd, e = exit): ")
 
-# if c, read triple.txt and print       
-elif user_input.lower() == "c":
-    with open("triple.txt", "r") as triple_text:
-        for line in triple_text:
-            print(line.strip())
-    
+    # if s, read double.txt and print
+    if user_input.lower() == "s":
+        with open("double.txt", "r") as double_text:
+            for line in double_text:
+                print(line.strip())
+
+    # if c, read triple.txt and print       
+    elif user_input.lower() == "c":
+        with open("triple.txt", "r") as triple_text:
+            for line in triple_text:
+                print(line.strip())
+        
     # if e, close
+    elif user_input.lower() == "e":
+        print("\nThanks for using this program\n")
+        break
+
+    # else, invalid input
+    else:
+        print("\nInvalid Input\n")
+
 # END OF PROGRAM
