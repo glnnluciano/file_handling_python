5a49fa7e9d38e1b21317df48adb6936209a8dbf5
glnnluciano <glnnluciano@gmail.com>
2024-04-25 13:43:23 +0800

    if even, square the number

--- squared_even_and_cubed_odd/squared_even_and_cubed_odd.py
+++ squared_even_and_cubed_odd/squared_even_and_cubed_odd.py
@@ -4,8 +4,13 @@ with open("integers.txt", "r") as integer_file:
 
 # convert line into integer
         line = int(line.strip())
-        
+
 # if number is even, square it and append in double.txt
+        if int(line) % 2 == 0:
+
+            # square the even integer
+            line = line**2
+
     # open and append line to double.txt
 # if number is odd, cube it and append in triple.txt
     # open and append line to triple.txt 
